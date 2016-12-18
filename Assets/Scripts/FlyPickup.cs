789ce9df4b286068348c8675a6faae64498461e8
chadroffey <chadroffey@Chads-MacBook-Pro-2.local>
2016-12-17 18:06:52 -0800

    Adding destroy on fly object collider and magic particles

---
+++ Assets/Scripts/FlyPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyPickup : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject pickupPrefab;
+
+	void OnTriggerEnter(Collider other) {
+
+		//If collider other is tagged tagged with "player"
+		if (other.CompareTag ("Player")) {
+
+			//...add the pickup particles...
+			Instantiate (pickupPrefab, transform.position, Quaternion.identity);
+
+			Destroy (gameObject);
+
+		}
+	}
+
+}
---
+++ Assets/Scripts/PickupPatriclesDestruction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupPatriclesDestruction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		// Destroy the "pickkup particles" after 5 seconds
+		Destroy (gameObject, 5f);
+
+		}
+}
